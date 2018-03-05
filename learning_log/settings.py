5f5a1e59ecca76ffb438cce849cd343eb7550a77
Paul Kitonyi <paulmucimah@gmail.com>
2018-03-05 12:28:23 +0300

    up to date

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # My apps 
+    # My apps
+    'learning_logs', 
 ]
 
 MIDDLEWARE = [
