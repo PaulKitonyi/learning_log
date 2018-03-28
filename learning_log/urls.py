f56b5ac05576606e3cbaa4c583e1e08d62702d12
Paul Kitonyi <paulmucimah@gmail.com>
2018-03-28 11:52:45 +0300

    update

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings 
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
+from learning_logs.views import index, about_us, contact_us, topics, topic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),
+    path('topics',topics),
+    path('topic',topic),
+    path('about',about_us),
+    path('contact',contact_us),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,30 @@
+from .models import Topic, Entry
+from  django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """Return home page for learning logs"""
+    return render(request,'index.html')
+
+def topics(request):
+    """Return Stored Topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request,'topics.html',context)
+
+def topic(request, topic_id):
+    """Return Stored Topics and all its Entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request,'topic.html',context)
+
+def about_us(request):
+    """Return About Us Page"""
+    return render(request,'about_us.html')
+
+def contact_us(request):
+    """Return Contact Us Page."""
+    return render(request,'contact_us.html')
